393de47949b8e858770bba5f633568249961396e
SaymanDotNet <saymannsk@gmail.com>
2025-03-13 11:26:43 +0700

    Добавьте файлы проекта.

---
+++ StringSplit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringSplit
+{
+	internal class HomeWork_207
+	{
+		static void Main(string[] args)
+		{
+			string words = "Дана строка с текстом, используя метод строки String.Split() получить массив слов, которые разделены пробелом в тексте и вывести массив, каждое слово с новой строки.";
+			string[] arrayWords;
+			char delimiter = ' ';
+
+			arrayWords = words.Split(new char[] { delimiter });
+
+			foreach (string word in arrayWords)
+			{
+				Console.WriteLine(word);
+			}
+		}
+	}
+}
